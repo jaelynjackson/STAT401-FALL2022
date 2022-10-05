354ba4c0e4b76dc54e40132a1d33a537289a1b08
jaelynjackson <89201162+jaelynjackson@users.noreply.github.com>
2022-10-05 11:45:56 -0400

    Add files via upload

---
+++ Exam1.R
@@ -0,0 +1,13 @@
+#Author: Jackson, Jaelyn Date: 10/05/22, Purpose: Calculate Paired T-test 
+
+x = c(1000,230,12333,3455,23,12,3,4,45,56,78)
+y = c(1,2,3,4,5,6,7,8,9,10,11)
+
+pts = seq(-4.5,4.5,length=100)
+plot(pts,dt(pts,df=9),col='red',type='l')
+
+lines(density(x), col='green')
+lines(density(y), col='blue')
+
+ttest = t.test(x,y)
+ttest
